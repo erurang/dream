d72b0b8316d16294dceab050e025d84553ebbf2d
erurang <56789064+erurang@users.noreply.github.com>
2021-07-15 01:01:08 +0900

    middleware

--- nodejs/node-server/express/app.js
+++ nodejs/node-server/express/app.js
@@ -4,53 +4,37 @@ import express from "express";
 
 const app = express();
 
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("1");
+    // next가 없으면 다음으로 실행이안댐.
+    // next() // 1 1-1
+    // route 로 지정하면 같은 url에서 아래의 1-1은 실행되지않고 다음(2) 로 넘어감
+    // next('route')
+    // 에러로 처리하며 맨아래의 use에서 처리댐
+    next(new Error('error'))
+  },
+  (req, res, next) => {
+    console.log("1-1");
+    // res.send('1-1 over')
+    // next() // 1 1-1 2
+  }
+);
+
 app.get("/", (req, res, next) => {
-  //   console.log(req.path); // /
-  //   console.log(req.headers); // header
-  console.log(req.params); // {}
-  console.log(req.query); // {}
-
-  res.end("hi");
+  console.log("2");
 });
 
-app.get("/params/:id", (req, res, next) => {
-  // console.log(req.path); // /params
-  // console.log(req.headers); // header
-  console.log(req.params); // {id: '내가 url에 친것'}
+// error
+app.use((req,res,next) => {
+    res.status(404).send('not available')
+})
 
-  console.log(req.params.id); // 변수접근가능
-
-  res.end("hi");
-});
-
-app.get("/query", (req, res, next) => {
-  // console.log(req.path); // /params
-  // console.log(req.headers); // header
-  // http://localhost:8080/query?key=zz
-  console.log(req.query); // => { key : "zz"}
-  console.log(req.query.key); // => zz
-  res.end("hi");
-});
+app.use((error,req,res,next) => {
+    console.error(error)
+    res.status(500).send('sorry try later')
+})
 
 
-app.get("/json", (req, res, next) => {
-    res.json({name:'zz'})
-  });
-
-
-  app.get("/statuscode", (req, res, next) => {
-    res.sendStatus(400)
-  });
-
-  app.get("/statuscode2", (req, res, next) => {
-    res.status(200).send('ok')
-  });
-
-
-  // network탭 header에서 key : 'value' 확인가능.
-  
-  app.get("/setheader", (req, res, next) => {
-    res.setHeader('key','value')
-    res.status(200).end()
-  });
 app.listen(8080);
