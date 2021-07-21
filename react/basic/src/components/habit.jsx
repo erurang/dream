dabd2e93deefad41353edb2d1d66387d677e09a3
erurang <56789064+erurang@users.noreply.github.com>
2021-07-21 20:49:03 +0900

    함수 끌어올리기 (이벤트관리)

--- react/basic/src/components/habit.jsx
+++ react/basic/src/components/habit.jsx
@@ -2,35 +2,24 @@ import React, { Component } from 'react';
 
 class Habit extends Component {
 
-    state = {
-        count: 0
-    }
-
-    handleIncrement = e => {
-        // this.state.count++ // 불변성을 지켜야함
-        this.setState({count: this.state.count+1})
-    }
-
-    handleDecrement = e => {
-        const count = this.state.count - 1
-        this.setState({count: count < 0 ? 0 : count})
-    }
-
-    handleDelete = e => {
-        
-    }
+    // 데이터를 전달받아서 표현만할뿐 컴포넌트 자체에서 상태를 가지고있는것은 좋지못함
+    // 상태를 끌어올려줘야함
+    // state = {
+    //     count: 0
+    // }
 
     render() {
         // props를 통해 받아온 habit들은 this.props에 저장이됨
-        console.log(this.props);
+        // console.log(this.props);
         const {name,count} = this.props.habit
+        
 
         return <li className="habit">
             <span className="habit-name">{name}</span>
             <span className="habit-count">{count}</span>
-            <button className="habit-button habit-increase" onClick={this.handleIncrement}>+</button>
-            <button className="habit-button habit-decrease" onClick={this.handleDecrement}>-</button>
-            <button className="habit-button habit-delete" onClick={this.handleDelete}> x</button>
+            <button className="habit-button habit-increase" onClick={() => this.props.handleIncrement(this.props.habit)}>+</button>
+            <button className="habit-button habit-decrease" onClick={() => this.props.handleDecrement(this.props.habit)}>-</button>
+            <button className="habit-button habit-delete" onClick={() => this.props.handleDelete(this.props.habit)}> x</button>
         </li>
     }
 }
--- react/basic/src/components/habits.jsx
+++ react/basic/src/components/habits.jsx
@@ -3,17 +3,37 @@ import Habit from "./habit"
 
 class Habits extends Component {
 
+    handleIncrement = e => {
+        console.log(e, '증가');
+    }
+
+    handleDecrement = e => {
+        console.log(e, '감소');
+    }
+
+    handleDelete = e => {
+        console.log(e, '삭제');
+    }
+
     state = {
-        habits : [
-            {name: 'reading', count : 0, id:1},
-            {name: 'running', count : 0, id:2},
-            {name: 'coding', count : 0, id:3},
+        habits: [
+            { name: 'reading', count: 0, id: 1 },
+            { name: 'running', count: 0, id: 2 },
+            { name: 'coding', count: 0, id: 3 },
         ]
     }
 
     render() {
         return <ul>
-            {this.state.habits.map(habit => <Habit habit={habit} key={habit.id}/>)}
+            {this.state.habits.map(habit =>
+                <Habit
+                    key={habit.id}
+                    habit={habit}
+                    handleIncrement={this.handleIncrement}
+                    handleDecrement={this.handleDecrement}
+                    handleDelete={this.handleDelete}
+                />
+            )}
         </ul>
     }
 }
