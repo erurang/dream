e366a6a76dc3795b19c8477ce460616e6977f090
erurang <56789064+erurang@users.noreply.github.com>
2021-07-21 17:13:16 +0900

    react basic file

--- react/basic/src/app.jsx
+++ react/basic/src/app.jsx
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>dd</h1>
+      <h1>Hi</h1>
     </div>
   );
 }
