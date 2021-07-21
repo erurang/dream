7beed7e7288c0b88903cac1d82c5ba0f9dc2ac4b
erurang <56789064+erurang@users.noreply.github.com>
2021-07-21 18:45:50 +0900

    count를 통한 state 관리

--- react/basic/src/components/habit.jsx
+++ react/basic/src/components/habit.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+
+    state = {
+        count: 0
+    }
+
+    handleIncrement = e => {
+        // this.state.count++ // 불변성을 지켜야함
+        this.setState({count: this.state.count+1})
+    }
+
+    handleDecrement = e => {
+        const count = this.state.count - 1
+        this.setState({count: count < 0 ? 0 : count})
+    }
+
+    handleDelete = e => {
+        
+    }
+
     render() {
         return <li className="habit">
             <span className="habit-name">Reading</span>
-            <span className="habit-count">8</span>
-            <button className="habit-button habit-increase">+</button>
-            <button className="habit-button habit-decrease">-</button>
-            <button className="habit-button habit-delete">x</button>
+            <span className="habit-count">{this.state.count}</span>
+            <button className="habit-button habit-increase" onClick={this.handleIncrement}>+</button>
+            <button className="habit-button habit-decrease" onClick={this.handleDecrement}>-</button>
+            <button className="habit-button habit-delete" onClick={this.handleDelete}> x</button>
         </li>
     }
 }
