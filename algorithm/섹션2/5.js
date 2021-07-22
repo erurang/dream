405e6430e80ec405137f2762bc0a7052c107d52b
erurang <56789064+erurang@users.noreply.github.com>
2021-07-23 03:07:38 +0900

    Array.from({length:3},() => 0) 배열초기화

--- algorithm/섹션2/5.js
+++ algorithm/섹션2/5.js
@@ -1,7 +1,34 @@
-function solution(n,arr) {
+function solution(n, arr) {
+  //   let rate = [];
 
-}   
+  //   for (let i = 0; i < n; i++) {
+  //     rate.push([arr[i], i]);
+  //   }
 
-console.log(solution(5,[87,89,92,100,76])) // 4 3 2 1 5
-console.log(solution(5,[87,87,92,100,76]))
+  //   for (let i = 0; i < n; i++) {
+  //     let cnt = n;
 
+  //     for (let j = 0; j < n; j++) {
+  //       if (i === j) continue;
+  //       if (rate[i][0] > rate[j][0]) cnt--;
+  //       if (rate[i][0] === rate[j][0]) cnt--;
+  //     }
+
+  //     rate[i][1] = cnt;
+  //   }
+
+  //   console.log(rate);
+  let n = arr.length;
+  
+  let answer = Array.from({ length: n }, () => 1);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[j] > arr[i]) answer[i]++;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(5, [87, 89, 92, 100, 76])); // 4 3 2 1 5
+console.log(solution(5, [87, 87, 87, 100, 76]));
