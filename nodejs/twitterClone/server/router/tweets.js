7852a3baf7a7ac4ca7cbbe105d4844955729408f
erurang <56789064+erurang@users.noreply.github.com>
2021-07-22 23:56:35 +0900

    validator route적용

--- nodejs/twitterClone/server/router/tweets.js
+++ nodejs/twitterClone/server/router/tweets.js
@@ -1,22 +1,35 @@
-import express from 'express';
-import 'express-async-errors';
-import * as tweetController from '../controller/tweet.js';
+import express from "express";
+import "express-async-errors";
+import { body } from "express-validator";
+
+import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
+
 const router = express.Router();
+
+const validateTweet = [
+  body("text").trim().isLength({ min: 3 }).withMessage("최소 3글자이상!"),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
-router.get('/', tweetController.getTweets);
+router.get("/", tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', tweetController.getTweet);
+router.get("/:id", tweetController.getTweet);
 
 // POST /tweeets
-router.post('/', tweetController.createTweet);
+router.post(
+  "/",
+  validateTweet,
+  tweetController.createTweet
+);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put("/:id", validateTweet,tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete('/:id', tweetController.deleteTweet);
+router.delete("/:id", tweetController.deleteTweet);
 
 export default router;
