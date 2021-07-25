105831bb34374c544fc1e53b4e1148f985de3bd7
erurang <56789064+erurang@users.noreply.github.com>
2021-07-26 01:38:08 +0900

    graphql-tools를 통한 schema(resolver,typedf,mutation)분리후 다시 합치기

--- react/Instagram/backend/server.js
+++ react/Instagram/backend/server.js
@@ -1,48 +1,8 @@
-import { ApolloServer, gql } from "apollo-server";
-import { PrismaClient } from "@prisma/client";
-
-const client = new PrismaClient();
-
-const typeDefs = gql`
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-  },
-  Mutation: {
-    createMovie: (root, { title, year, gerne }, context, info) =>
-      client.movie.create({ data: { title, year, gerne } }),
-    deleteMovie: (root, { id }, context, info) =>
-      client.movie.delete({ where: { id } }),
-    updateMovie: (_, { id, year }) =>
-      client.movie.update({ where: { id }, data: { year } }),
-  },
-};
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema
 });
 
 server
