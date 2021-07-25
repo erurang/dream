9ec7bc752696c0d11046a6e5b0863352befde87c
erurang <56789064+erurang@users.noreply.github.com>
2021-07-26 04:47:43 +0900

    edit Profile hashing password

--- react/Instagram/backend/users/editProfile/editProfile.resolvers.js
+++ react/Instagram/backend/users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,36 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
-    Mutation : {
-        editProfile: () => console.log('hi')
-    }
-}
+  Mutation: {
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      let uglyPassword = null;
+
+      if (password) {
+        uglyPassword = await bcrypt.hash(password, 10);
+      }
+
+      const updatedUser = await client.user.update({
+        where: {
+          // after edit
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password : uglyPassword }),
+        },
+      });
+
+
+      if(updatedUser.id) return {ok:true}
+      else return {ok:false, error:"Could not update profile"}
+      
+    },
+  },
+};
