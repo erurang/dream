20740371b55333564311c59288201eb3473718ae
erurang <56789064+erurang@users.noreply.github.com>
2021-07-26 23:49:17 +0900

    graphql이 여러파일로 갈라진 schema를 Graph-tool로 생성 결합 방식에서 apollo server에서 schema를 생성하는것으로 변경

--- react/Instagram/backend/server.js
+++ react/Instagram/backend/server.js
@@ -1,15 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { ApolloServer } from "apollo-server";
-import schema from "./schema";
-import { getUser,protectResolver } from "./users/users.utils";
+import { typeDefs, resolvers } from "./schema";
+import { getUser, protectResolver } from "./users/users.utils";
 
 const server = new ApolloServer({
-  schema,
+  typeDefs,
+  resolvers,
   // query를 구성할때의 인자 (root,args,context,info) 중 Context는 모든 Resolver에서 접근이 가능하도록 해줌
   context: async ({ req, res }) => {
     // console.log(req.headers);
-    return { loggedInUser: await getUser(req.headers.token),protectResolver };
+    return { loggedInUser: await getUser(req.headers.token), protectResolver };
   },
 });
 
@@ -18,4 +19,3 @@ const PORT = process.env.PORT;
 server
   .listen(PORT)
   .then(() => console.log(`server is running ong http:/localhsot:${PORT}`));
-
