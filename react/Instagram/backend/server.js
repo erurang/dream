6ac2c7d0a3e61773628b146b56030dae33f3ec25
erurang <56789064+erurang@users.noreply.github.com>
2021-07-27 03:23:27 +0900

    apolloserver 위에 express 적용

--- react/Instagram/backend/server.js
+++ react/Instagram/backend/server.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utils";
-
+import express from "express"
+import morgan from "morgan"
 
 const PORT = process.env.PORT;
 
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   // query를 구성할때의 인자 (root,args,context,info) 중 Context는 모든 Resolver에서 접근이 가능하도록 해줌
@@ -16,6 +17,15 @@ const server = new ApolloServer({
     return { loggedInUser: await getUser(req.headers.token), protectResolver };
   },
 });
-server
-  .listen(PORT)
-  .then(() => console.log(`server is running ong http:/localhsot:${PORT}`));
+
+const app = express();
+
+app.use(morgan('tiny') )
+
+app.use(express.static("uploads"))
+
+// 아폴로서버에 express서버와 함께 작동하도록 넘김
+apollo.applyMiddleware({app})
+
+app
+  .listen({port:PORT}, () => console.log(`server is running ong http:/localhsot:${PORT}`))
