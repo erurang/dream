4f0833346101d408d64d2ee4e711eb45be8656dd
erurang <56789064+erurang@users.noreply.github.com>
2021-08-01 20:30:14 +0900

    protetedResolver use the info argument

--- react/Instagram/backend/users/users.utils.js
+++ react/Instagram/backend/users/users.utils.js
@@ -26,6 +26,14 @@ export const getUser = async (token) => {
 
 export function protectedResolver(ourResolver) {
   return function (root, args, context, info) {
+    // console.log(info)
+    // 여기서 info의 operation.을 보면 우리가 query/mutation 요청을 확인할수있음.
+    // 만약 seeFeed의 경우. [Photo]를 반환하지만, resolver에는 protectedResolver로 묶여서 로그인시에만 확인할수 있도록 되어있음.
+    // 만약 로그인 안한사람이 seeFeed를 하면 어떻게되나..? 그럼 => 뻑남 왜냐? 우리는 [Photo]를 반환한다고 말했기 떄문..
+    // 그래서 여기서 한번더 조정해준다
+
+    const query = info.operation.operation === "query";
+    if (query) return null;
     if (!context.loggedInUser) return { ok: false, error: "Please login" };
     return ourResolver(root, args, context, info);
   };
