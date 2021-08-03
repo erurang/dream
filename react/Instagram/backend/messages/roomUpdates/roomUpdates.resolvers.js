8a5ffe3945bcc78a9b5ab558483e0421e1efe223
erurang <56789064+erurang@users.noreply.github.com>
2021-08-03 20:51:13 +0900

    withFilter()를 통한 트리거이벤트 필터링하기

--- react/Instagram/backend/messages/roomUpdates/roomUpdates.resolvers.js
+++ react/Instagram/backend/messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,3 +1,4 @@
+import { withFilter } from "apollo-server";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
@@ -6,7 +7,34 @@ export default {
     roomUpdates: {
       // trigger를 listen 하게해줌
       // New_MESSAGE 트리거가 발생하면 반응하게사는뜻.
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      //   subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      // 첫인자는 트리거, 두번째 인자는 true를 반환하면 업데이트를함
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(NEW_MESSAGE),
+        (payload, variables) => {
+          /*
+            console.log(payload, variables);
+
+            payload
+            {
+            roomUpdates: {
+                id: 12,
+                createdAt: 2021-08-03T11:43:56.180Z,
+                updatedAt: 2021-08-03T11:43:56.181Z,
+                message: '펍섭 테스트4',
+                userId: 2,
+                roomId: 9,
+                read: false
+            }
+            } 
+            variables
+            { id: 9 }
+            */
+
+          // 여기서 우리는 구독을할때 특정방에 구독을 하고, 그후에 아래조건에 부합한다면. 그때 우리는 이벤트를 실행시킴.
+          return payload.roomUpdates.roomId === variables.id;
+        }
+      ),
       /**
        * 그런데 이대로 subscription {
        *                roomUpdates{
--- react/Instagram/backend/messages/roomUpdates/roomUpdates.typeDefs.js
+++ react/Instagram/backend/messages/roomUpdates/roomUpdates.typeDefs.js
@@ -2,6 +2,10 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Subscription {
-    roomUpdates: Message
+    #   이렇게하면 우린 모든이벤트를 다 받게됨
+    # roomUpdates: Message
+    # 그래서 이러게 조정해준다
+    # 우리가 받고싶어하는 대화방만 구독하겠다.
+    roomUpdates(id: Int!): Message
   }
 `;
