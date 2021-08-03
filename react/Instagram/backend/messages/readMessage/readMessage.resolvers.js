a1dd56268ab494a1b656c3bc1e822d45738ab404
erurang <56789064+erurang@users.noreply.github.com>
2021-08-03 18:22:43 +0900

    message model test & bug fix

--- react/Instagram/backend/messages/readMessage/readMessage.resolvers.js
+++ react/Instagram/backend/messages/readMessage/readMessage.resolvers.js
@@ -4,15 +4,22 @@ import { protectedResolver } from "../../users/users.utils";
 export default {
   Mutation: {
     readMessage: protectedResolver(async (_, { id }, { loggedInUser }) => {
+      // id는 메세지의 id
       const message = await client.message.findFirst({
         where: {
+          // 메세지 id의 테이블에
           id,
+          // 유저아이디가
           userId: {
+            // 현재접속한 유저아이디가 아닌 걸고르고
             not: loggedInUser.id,
           },
+          // 대화방에서
           room: {
+            // 유저안에
             users: {
               some: {
+                // 나인걸 고른다.
                 id: loggedInUser.id,
               },
             },
