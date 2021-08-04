756b7995ee938be89fc3f4f906c24fa1cf544209
erurang <56789064+erurang@users.noreply.github.com>
2021-08-04 19:57:36 +0900

    간단한 screen component

--- react/Instagram/frontend/src/App.js
+++ react/Instagram/frontend/src/App.js
@@ -1,25 +1,21 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-
-// brouserRouter와의 차이는 url에 #의 존재와, 배포할때의 차이, brouserRouter는 배포할때 다른 설정을 더 해줘야함.
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
+  // 로그인을 흉내내기위한 임시변수
+  const isLoggedIn = true;
+
   return (
     <div className="App">
       <Router>
-        {/* http://localhost:3000/#/ */}
-        {/* /로시작하는걸 모두 렌더하는걸 막기위해, 한url에 하나만 렌더하는 switch사용 */}
         <Switch>
-          {/* 이렇게 하면 우리는 / 로시작하는 모든 라우터에 홈이 적히게됨 */}
           <Route path="/" exact>
-            <h1>home</h1>
+            {isLoggedIn ? <Home /> : <Login />}
           </Route>
-          {/* http://localhost:3000/#/potato */}
-          <Route path="/potato">
-            <h1>potato</h1>
-          </Route>
-          {/* http://localhost:3000/#/banana */}
-          <Route path="/banana">
-            <h1>banana</h1>
+          <Route>
+            <NotFound />
           </Route>
         </Switch>
       </Router>
