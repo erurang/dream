37f66d913a2749c1649022e77f9ba1a19cb215a5
erurang <56789064+erurang@users.noreply.github.com>
2021-08-11 00:55:58 +0900

    photo container

--- react/Instagram/frontend/src/components/Avatar.js
+++ react/Instagram/frontend/src/components/Avatar.js
@@ -1,9 +1,9 @@
 const { default: styled } = require("styled-components");
 
 const SAvatar = styled.div`
-  width: 25px;
-  height: 25px;
-  border-radius: 15px;
+  width: ${(props) => (props.lg ? "30px" : "25px")};
+  height: ${(props) => (props.lg ? "30px" : "25px")};
+  border-radius: 50%;
   background-color: #2c2c2c;
   overflow: hidden;
 `;
@@ -11,8 +11,8 @@ const Img = styled.img`
   max-width: 100%;
 `;
 
-function Avatar({ url = "" }) {
-  return <SAvatar>{url !== "" ? <Img src={url}></Img> : null}</SAvatar>;
+function Avatar({ url = "", lg = false }) {
+  return <SAvatar lg={lg}>{url !== "" ? <Img src={url}></Img> : null}</SAvatar>;
 }
 
 export default Avatar;
