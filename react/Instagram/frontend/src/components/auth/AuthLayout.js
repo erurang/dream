e4dc59899ee5f76f818ad157dc38a0fb972ca24b
erurang <56789064+erurang@users.noreply.github.com>
2021-08-10 20:30:29 +0900

    darkmode

--- react/Instagram/frontend/src/components/auth/AuthLayout.js
+++ react/Instagram/frontend/src/components/auth/AuthLayout.js
@@ -1,4 +1,8 @@
+import { useReactiveVar } from "@apollo/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
+import { darkModeVar, disableDarkMode, enableDarkMode } from "../../apollo";
+import { faSun, faMoon } from "@fortawesome/free-regular-svg-icons";
 
 // 로그인화면 / 가입화면에서 공통적으로 쓰일수있는 부분을 컴포넌트로 뻄
 
@@ -15,10 +19,25 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
+const Footer = styled.footer`
+  margin-top: 20px;
+`;
+
+const DarkModeBtn = styled.button`
+  cursor: pointer;
+`;
+
 function AuthLayout({ children }) {
+  const darkMode = useReactiveVar(darkModeVar);
+
   return (
     <Container>
       <Wrapper>{children}</Wrapper>
+      <Footer>
+        <DarkModeBtn onClick={darkMode ? disableDarkMode : enableDarkMode}>
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
+        </DarkModeBtn>
+      </Footer>
     </Container>
   );
 }
