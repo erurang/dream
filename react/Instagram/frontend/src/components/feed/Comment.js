4d5a52eed243092e67919fdd699d3a46191cbdcc
erurang <56789064+erurang@users.noreply.github.com>
2021-08-11 19:07:11 +0900

    sanitzie를 쓰지않고 react component link를 html코드로 넣기위한 정규식방법

--- react/Instagram/frontend/src/components/feed/Comment.js
+++ react/Instagram/frontend/src/components/feed/Comment.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 import { FatText } from "../shared";
-import sanitizeHtml from "sanitize-html";
-
+import { Link } from "react-router-dom";
+import React from "react";
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
-  mark {
+  a {
     background-color: inherit;
     color: ${(props) => props.theme.accent};
     cursor: pointer;
@@ -16,18 +16,29 @@ const CommentCaption = styled.span`
 `;
 
 function Comment({ author, caption }) {
-  const cleanedCaption = sanitizeHtml(
-    caption.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
-    { allowedTags: ["mark"] }
-  );
+  // const cleanedCaption = sanitizeHtml();
+  // 이방법도 좋지만 우리는 html을 링크로 넘기는것이기때문에..
+  // 리액트에서 컴포넌트를 넘길수가없음. 왜냐 html만 파싱하니까
+  // 이런방법도 있따는건 알아두면댐
+  // caption.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
+  // { allowedTags: ["mark"] }
+
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: cleanedCaption,
-        }}
-      />
+      <CommentCaption>
+        {caption.split(" ").map((word, i) =>
+          /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/.test(word) ? (
+            <React.Fragment key={i}>
+              <Link key={i} to={`/hashtags/${word}`}>
+                {word}
+              </Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={i}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
