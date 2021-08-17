2fbc1e129f67103515fcf3cf78b7f8824542d0e2
erurang <56789064+erurang@users.noreply.github.com>
2021-08-17 20:07:47 +0900

    m

---
+++ typescript/7-motion-project/src/components/page/item/note.ts
@@ -0,0 +1,6 @@
+import { BaseComponent } from "./../../components.js";
+export class NoteComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, body: string) {
+    super();
+  }
+}
