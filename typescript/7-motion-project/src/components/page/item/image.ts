b6084698fe7d6c6b03ef4225254a4f19b6c979c5
erurang <56789064+erurang@users.noreply.github.com>
2021-08-17 17:22:58 +0900

    refactor

--- typescript/7-motion-project/src/components/page/item/image.ts
+++ typescript/7-motion-project/src/components/page/item/image.ts
@@ -1,25 +1,15 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from "./../../components.js";
 
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement("template");
-
-    // 여기에 바로 ${title} ${url}을 넣어서 쓰는 방법도 있지만
-    // 만약 유저가 html js를 인자로 주게되면 유저가 조작하는 사태가 발생함
-    // 만약 title에 <p>zz</p>를 넣으면 zz가 나오는 사태가 발생함
-    // 이런걸 미연에 방지하기위해
-    template.innerHTML = `
-    <section class="image">
-        <div class="image__holder">
-            <img class="image__thumbnail">
-        </div>
-        <p class="image__title"></p>
-    </section>
-    `;
-
-    // 하나하나 접근하여 처리함
-    this.element = template.content.firstElementChild as HTMLElement;
-
+    super(`
+      <section class="image">
+          <div class="image__holder">
+              <img class="image__thumbnail">
+          </div>
+          <p class="image__title"></p>
+      </section>
+      `);
     const imageElement = this.element.querySelector(
       ".image__thumbnail"
     ) as HTMLImageElement;
@@ -30,9 +20,5 @@ export class ImageComponent {
       ".image__title"
     ) as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
--- typescript/7-motion-project/src/components/page/page.ts
+++ typescript/7-motion-project/src/components/page/page.ts
@@ -1,14 +1,7 @@
-export class PageComponent {
-  private element: HTMLUListElement;
-  // 생성시에 ul이 만들어짐
-  constructor() {
-    this.element = document.createElement("ul");
-    this.element.setAttribute("class", "page");
-    this.element.textContent = "this is page component";
-  }
+import { BaseComponent } from "./../components.js";
 
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    // 부모컨테이너에 추가함,
-    parent.insertAdjacentElement(position, this.element);
+export class PageComponent extends BaseComponent<HTMLUListElement> {
+  constructor() {
+    super("<ul class='page'>This is PageComponent</ul>");
   }
 }
