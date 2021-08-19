0b360e4f814c4d7a2ef3cfd87ace43ac9a2eaa2e
erurang <56789064+erurang@users.noreply.github.com>
2021-08-19 23:38:32 +0900

    module refactor

--- nest-base/src/app.module.ts
+++ nest-base/src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  // nest g co 로 컨트롤러 파일을 자동생성했는데 여기에 자동추가된걸 볼수있음
-  controllers: [MoviesController],
-  // nest g s 로 자동생성됨
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  // app.module은 하나의 컨트롤러와 하나의 프로바이더만 가지고있어야한다.
+  // 즉 지금은 movies 하나를 만들어서 썻지만. 만약에 다른걸 만들게되면..? 이렇게 적용할수없다.
+  // 그래서 MoviesService MoviesController를 movie라는 모듈로 만들어서 import시켜야한다.
+  // nest g mo => imports 자동적용
+  // 그럼 컨트롤러랑 프로바이더는 언제쓰지..? nest g co
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
