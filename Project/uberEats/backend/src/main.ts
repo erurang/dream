3ee18229c04ec808adfcb391e5628cc9ffc7be85
erurang <56789064+erurang@users.noreply.github.com>
2021-08-20 18:14:29 +0900

    validation을 위한 class-validator class-transformer설치

--- Project/uberEats/backend/src/main.ts
+++ Project/uberEats/backend/src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   // appmodule은 main.ts로 imports되는 유일한 모듈임
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- Project/uberEats/backend/src/restaurants/dtos/create-restaurant.dto.ts
+++ Project/uberEats/backend/src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,14 +1,22 @@
 import { ArgsType, Field } from '@nestjs/graphql';
 
+import { IsString, Length } from 'class-validator';
 // @InputType() // 차이생각하기
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field(() => Boolean)
   isVegan: boolean;
+
   @Field(() => String)
+  @IsString()
   address: string;
+
   @Field(() => String)
+  @IsString()
   ownerName: string;
 }
