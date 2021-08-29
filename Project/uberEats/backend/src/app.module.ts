686e4ef271ab8251bec647ad006330b6776e2aa8
erurang <56789064+erurang@users.noreply.github.com>
2021-08-29 21:16:29 +0900

    typeOrm 설치와 연결

--- Project/uberEats/backend/src/app.module.ts
+++ Project/uberEats/backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
+import { TypeOrmModule } from "@nestjs/typeorm"
+
 @Module({
   imports: [
     GraphQLModule.forRoot({
@@ -10,8 +12,22 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      // typeorm과 연결하기위해
+
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: 'erurang',
+      // password : localhost일경우는 생략가능
+      database: 'uber-eats',
+      // typeorm이 db로 연결할때 db를 모듈의 상태로 마이그레이션함
+      synchronize: true,
+      // 데이터베이스 오류 로깅
+      logging: true
+    })
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
